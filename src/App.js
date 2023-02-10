cee76c15ec3d2b15e622ec84368c780eded526a8
Sannasi <sannasichottu@gmail.com>
2023-02-10 13:01:44 +0530

    home page setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <div className="App">
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      <Footer />
+     </div>
+    </Router>
   );
 }
 
---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Footer () {
+    return (
+        <footer className="py-1">
+            <p class="text-center text-dark mt-1">
+            © - 2022-2023, chottu.com,  All Rights Reserved
+      </p>
+    </footer>
+    )
+}
