ad49e32616feebad52a2d08850749cc41313dcb6
Sannasi <sannasichottu@gmail.com>
2023-02-10 21:47:12 +0530

    product listing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,34 @@
-import React, { Fragment, useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getProducts } from "../actions/productsActions"
-import MetaData from "./layouts/MetaData"
+import React, { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../actions/productsActions";
+import { Loader } from "./layouts/Loader";
+import MetaData from "./layouts/MetaData";
+import Product from "./product/Product";
 
-export default function Home () {
-
+export default function Home() {
   const dispatch = useDispatch();
-  const { products, loading } = useSelector((state) => state.productsState)
+  const { products, loading } = useSelector((state) => state.productsState);
 
-  useEffect(()=>{
-    dispatch(getProducts)
-  },[])
+  useEffect(() => {
+    dispatch(getProducts);
+  }, []);
 
-    return (
-
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-          <MetaData title={'Buy Best products'} />
-            <h1 id="products_heading">Latest Products</h1>
-                <section id="products" className="container mt-5">
-                  <div className="row">
-                    {products && products.map(product => (
-                      <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                      <div className="card p-3 rounded">
-                        <img
-                          className="card-img-top mx-auto"
-                          src={product.images[0].image} alt="product-img"
-                        />
-                        <div className="card-body d-flex flex-column">
-                          <h5 className="card-title">
-                            <a href="">{product.name}</a>
-                          </h5>
-                          <div className="ratings mt-auto">
-                              <div className="rating-outer">
-                                <div className="rating-inner" style={{width:`${product.ratings/5 * 100}%`}} ></div>
-                              </div>
-                            <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
-                          </div>
-                          <p className="card-text">${product.price}</p>
-                          <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-                        </div>
-                      </div>
-                    </div>
-                    ))}
-
-                  </div>
-                </section>
-            </Fragment>
-
-    )
+          <MetaData title={"Buy Best products"} />
+          <h1 id="products_heading">Latest Products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => <Product product={product} />)}
+            </div>
+          </section>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 }
