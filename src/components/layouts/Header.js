7832600db6224dad3898cd855964de6907adb99a
Sannasi <sannasichottu@gmail.com>
2023-02-10 23:30:22 +0530

    product details

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export default function Header () {
-    return (
-        <nav className="navbar row">
+export default function Header() {
+  return (
+    <nav className="navbar row">
       <div className="col-12 col-md-3">
         <div className="navbar-brand">
-            <img src="./images/logo.png" width="100px" alt='amazon logo' />
+          <img src="/images/logo.png" width="100px" alt="amazon logo" />
         </div>
       </div>
 
@@ -26,11 +26,17 @@ export default function Header () {
       </div>
 
       <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-        <button className="btn" id="login_btn">Login</button>
+        <button className="btn" id="login_btn">
+          Login
+        </button>
 
-        <span id="cart" className="ml-3">Cart</span>
-        <span className="ml-1" id="cart_count">2</span>
+        <span id="cart" className="ml-3">
+          Cart
+        </span>
+        <span className="ml-1" id="cart_count">
+          2
+        </span>
       </div>
     </nav>
-    )
+  );
 }
