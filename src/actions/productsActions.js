7a88cd13d853a80c0feba85b6605d861a9017503
Sannasi <sannasichottu@gmail.com>
2023-02-10 20:45:16 +0530

    product list with redux

--- src/actions/productsActions.js
+++ src/actions/productsActions.js
@@ -2,12 +2,11 @@ import axios from "axios"
 import { productsFail, productsRequest, productsSuccess } from "../slices/productsSlice"
 
 
-
-export const getsProducts = async (dispatch) => {
-    try {
+export const getProducts = async (dispatch) => {
+    try{
         dispatch(productsRequest())
-        const { data } = await axios.get(`http://localhost:3000/api/v1/products`);
-        dispatch (productsSuccess(data))
+        const { data } = await axios.get('/api/v1/products');
+        dispatch(productsSuccess(data))
     }catch (error) {
         //handle error
         dispatch(productsFail(error.response.data.message))
