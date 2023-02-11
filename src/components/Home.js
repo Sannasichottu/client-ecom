28629d3881c3ea477f414f5491851542de28ff6e
Sannasi <sannasichottu@gmail.com>
2023-02-11 19:55:01 +0530

    pagination

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,23 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { getProducts } from "../actions/productsActions";
 import { Loader } from "./layouts/Loader";
 import MetaData from "./layouts/MetaData";
 import Product from "./product/Product";
+import Pagination from "react-js-pagination";
 
 export default function Home() {
   const dispatch = useDispatch();
-  const { products, loading, error } = useSelector(
+  const { products, loading, error, productsCount, resPerPage } = useSelector(
     (state) => state.productsState
   );
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const setCurrentPageNo = (pageNo) => {
+    setCurrentPage(pageNo);
+  };
 
   useEffect(() => {
     if (error) {
@@ -19,7 +26,7 @@ export default function Home() {
       });
     }
     dispatch(getProducts);
-  }, [error]);
+  }, [error, dispatch, currentPage]);
 
   return (
     <Fragment>
@@ -32,9 +39,26 @@ export default function Home() {
           <section id="products" className="container mt-5">
             <div className="row">
               {products &&
-                products.map((product) => <Product product={product} />)}
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
             </div>
           </section>
+          {productsCount > 0 && productsCount > resPerPage ? (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                onChange={setCurrentPageNo}
+                totalItemsCount={productsCount}
+                itemsCountPerPage={resPerPage}
+                nextPageText={"Next..."}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass={"page-item"}
+                linkClass={"page-link"}
+              />
+            </div>
+          ) : null}
         </Fragment>
       )}
     </Fragment>
--- src/slices/productsSlice.js
+++ src/slices/productsSlice.js
@@ -1,30 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const productsSlice = createSlice({
-    name : 'products',
-    initialState : {
-        loading : false
+  name: "products",
+  initialState: {
+    loading: false,
+  },
+  reducers: {
+    productsRequest(state, action) {
+      return {
+        loading: true,
+      };
     },
-    reducers : {
-        productsRequest ( state, action) {
-            return {
-                loading : true,
-            }
-        },
-        productsSuccess ( state, action) {
-            return {
-                loading : false,
-                products : action.payload.products
-            }
-        },
-        productsFail ( state, action ) {
-            return {
-                loading : false,
-                error : action.payload
-            }
-        }
-    }
+    productsSuccess(state, action) {
+      return {
+        loading: false,
+        products: action.payload.products,
+        productsCount: action.payload.count,
+        resPerPage: action.payload.resPerPage,
+      };
+    },
+    productsFail(state, action) {
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    },
+  },
 });
 
 const { actions, reducer } = productsSlice;
