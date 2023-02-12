884a4aaaa970456379809d174c4171408bff761b
Sannasi <sannasichottu@gmail.com>
2023-02-12 20:09:12 +0530

    userProfile

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Search from "./Search";
 import { Dropdown, DropdownButton, Image } from "react-bootstrap";
 import { logout } from "../../actions/userActions";
@@ -8,6 +8,7 @@ import { logout } from "../../actions/userActions";
 export default function Header() {
   const { isAuthenticated, user } = useSelector((state) => state.authState);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const logoutHandler = () => {
     dispatch(logout);
@@ -42,6 +43,14 @@ export default function Header() {
               <span>{user.name}</span>
             </Dropdown.Toggle>
             <Dropdown.Menu>
+              <Dropdown.Item
+                onClick={() => {
+                  navigate("/myprofile");
+                }}
+                className="text-dark"
+              >
+                Profile
+              </Dropdown.Item>
               <Dropdown.Item onClick={logoutHandler} className="text-danger">
                 Logout
               </Dropdown.Item>
