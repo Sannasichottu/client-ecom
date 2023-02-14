cdea98b18d4a837eb305e2d8b3571c8dfbc0e68e
Sannasi <sannasichottu@gmail.com>
2023-02-14 12:28:55 +0530

    handle cart summary

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -55,7 +55,7 @@ export default function Cart() {
                   </div>
 
                   <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                    <p id="card_item_price">{item.price}</p>
+                    <p id="card_item_price">$ {item.price}</p>
                   </div>
 
                   <div className="col-4 col-lg-3 mt-4 mt-lg-0">
@@ -101,10 +101,20 @@ export default function Cart() {
             <h4>Order Summary</h4>
             <hr />
             <p>
-              Subtotal: <span className="order-summary-values">1 (Units)</span>
+              Subtotal:{" "}
+              <span className="order-summary-values">
+                {items.reduce((acc, item) => acc + item.quantity, 0)} (Units)
+              </span>
             </p>
             <p>
-              Est. total: <span className="order-summary-values">$245.67</span>
+              Est. total:{" "}
+              <span className="order-summary-values">
+                ${" "}
+                {items.reduce(
+                  (acc, item) => acc + item.quantity * item.price,
+                  0
+                )}
+              </span>
             </p>
 
             <hr />
